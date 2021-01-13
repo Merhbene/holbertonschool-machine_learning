5ea1506ff09745e408d4743cf2bc8d9d971f98a7
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-13 17:15:01 +0100

    Update 1-neuron.py

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -3,22 +3,25 @@ import numpy as np
 
 
 class Neuron:
-   def __init__(self,nx):
-       self.nx=nx
-       if type(self.nx)!=int:
-          raise Exception("nx must be an integer")
-       if self.nx<1 :
-          raise Exception("nx must be a positive integer")
-       self.__W=np.expand_dims(np.random.normal(size=self.nx),axis=0)
-       self.__b=0
-       self.__A=0
-   @property
-   def W(self):
-      return(self.__W)
-   @property  
-   def b(self):
-      return(self.__b)
-   @property
-   def A(self):
-      return(self.__A)
+    def __init__(self,nx):
+          self.nx=nx
+          if type(self.nx) != int:
+             raise Exception("nx must be an integer")
+          if self.nx<1 :
+             raise Exception("nx must be a positive integer")
+          self.__W = np.expand_dims(np.random.normal(size=self.nx), axis=0)
+          self.__b = 0
+          self.__A = 0
+      
+    @property
+    def W(self):
+        return(self.__W)
+   
+    @property  
+    def b(self):
+        return(self.__b)
+   
+    @property
+    def A(self):
+        return(self.__A)
     
