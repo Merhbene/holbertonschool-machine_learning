659670bc5ae32c086ffff938df4f32c9d8ffbbf0
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-13 16:54:38 +0100

    Update 0-neuron.py

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -10,7 +10,7 @@ class Neuron():
             raise TypeError("nx must be an integer")
         if self.nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.expand_dims(np.random.randn(self.nx), axis=0) 
+        self.W = np.expand_dims(np.random.randn(self.nx), axis=0)
         self.b = 0
-        self.A = 0
+        self.A = 0 
  
