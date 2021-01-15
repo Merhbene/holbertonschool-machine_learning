0c9767b138c89c97e12edf329e04567a69c5ce65
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 13:54:49 +0100

    Update 13-neural_network.py

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -91,8 +91,9 @@ class NeuralNetwork:
         """Updates"""
         self.__W2 = self.__W2 - (alpha * dw2)
         self.__b2 = self.__b2 - (alpha * db2)
+        dz1 = np.dot(self.__W2.T, A2 - Y) * A1 * (1 - A1)
 
-        dz1 = np.dot(self.__W2.T, dz2) * A1 * (1 - A1)
+        #dz1 = np.dot(self.__W2.T, dz2) * A1 * (1 - A1)
         dw1 = (1 / m)*np.matmul(dz1, X.T)
         db1 = (1 / m)*np.sum(dz1, axis=1, keepdims=True)
 
