df2b5f60410f6250a4ad722c069b70d923282f1d
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 09:58:42 +0100

    Update 2-neuron.py

--- supervised_learning/0x01-classification/2-neuron.py
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
-
-class Neuron:
-  def __init__(self,nx):
-    self.nx=nx
-    if type(self.nx)!=int:
-      raise Exception("nx must be an integer")
-    if self.nx<1 :
-      raise Exception("nx must be a positive integer")
-    self.__W=np.expand_dims(np.random.normal(size=self.nx),axis=0)
-    self.__b=0
-    self.__A=0
-  @property
-  def W(self):
-    return(self.__W)
-  @property  
-  def b(self):
-    return(self.__b)
-  @property
-  def A(self):
-    return(self.__A)
+""" new class """
+import numpy as np
 
 
-  def forward_prop(self, X):
-    #m=X.shape[1]
-    z=np.matmul(self.__W,X)+self.__b
-    self.__A=(1/(1+np.exp(-z)))
-    return  self.__A
+class Neuron():
+    """ Neuron class"""
+    def __init__(self, nx):
+        """ init function """
+        self.nx = nx
+        if type(self.nx) != int:
+            raise TypeError("nx must be an integer")
+        if self.nx < 1:
+            raise ValueError("nx must be a positive integer")
+        # Private instance attributes
+        self.__W = np.expand_dims(np.random.normal(size=self.nx), axis=0)
+        self.__b = 0
+        self.__A = 0
+
+    """Getter for W """
+    @property
+    def W(self):
+        return(self.__W)
+
+    """Getter for b """
+    @property
+    def b(self):
+        return(self.__b)
+
+    """Getter for A """
+    @property
+    def A(self):
+        return(self.__A)
+
+    """ Calculate the forward propagation of the neuron """
+    def forward_prop(self, X):
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = (1 / (1 + np.exp(-z)))
+        return  self.__A
