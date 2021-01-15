0d77f2134565ad86bec2a34439d062a5c8702116
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 16:19:19 +0100

    Update 25-one_hot_decode.py

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -4,14 +4,16 @@ import numpy as np
 
 
 def one_hot_decode(one_hot):
+   "Convert a one-hot matrix into a vector of labels"
    try:
-         classes, m = one_hot.shape
-         Y_decoded = np.zeros(shape = (m,))
+      classes, m = one_hot.shape
+      "classes is the maximum number of classes"
+      Y_decoded = np.zeros(shape = (m,))
 
-         for i in range(m):
-              for j in range(classes):
-                   if one_hot[j][i] == 1 :
-                        Y_decoded[i] = j
-         return Y_decoded.astype(int)
-   exept Exception :
+      for i in range(m):
+           for j in range(classes):
+                if one_hot[j][i] == 1 :
+                     Y_decoded[i] = j
+      return Y_decoded.astype(int)
+   except Exception:
       return None
