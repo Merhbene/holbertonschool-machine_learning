39f75f0212abeab1a8c07f4a4d245351c2bb64a6
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 16:03:30 +0100

    Update 24-one_hot_encode.py

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
+import numpy as np
+
 
 def one_hot_encode(Y, classes):
-  m=Y.shape[0]
-  Y_one_hot=np.zeros(shape=(classes, m))
-  for i in range(m) :
-    Y_one_hot[Y[i]][i]=1
+   """Convert a numeric label vector into a one-hot matrix"""
+    m = Y.shape[0]
+    Y_one_hot = np.zeros(shape=(classes, m))
+    for i in range(m) :
+      Y_one_hot[Y[i]][i] = 1
 
-  return Y_one_hot
-  
-
-
-
+    return Y_one_hot
