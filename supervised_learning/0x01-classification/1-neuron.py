f4493d5c24daa5022e1a7ef7cde8ac784b227a72
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 09:37:43 +0100

    Update 1-neuron.py

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -12,20 +12,19 @@ class Neuron():
         if self.nx < 1:
             raise ValueError("nx must be a positive integer")
         # Insert a new axis
-        self.__W = np.expand_dims(np.random.normal(size = self.nx), axis = 0)
+        self.__W = np.expand_dims(np.random.normal(size=self.nx), axis=0)
         self.__b = 0
         self.__A = 0
-        
-    #Private instance attributes
+
+    # Private instance attributes
     @property
     def W(self):
         return(self.__W)
-   
+
     @property  
     def b(self):
         return(self.__b)
-   
+
     @property
     def A(self):
         return(self.__A)
-    
