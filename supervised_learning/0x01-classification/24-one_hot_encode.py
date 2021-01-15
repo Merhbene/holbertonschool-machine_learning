b023951605533ddd0cffd4bed4c245b8600b6f4b
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 16:05:28 +0100

    Update 24-one_hot_encode.py

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -7,6 +7,6 @@ def one_hot_encode(Y, classes):
     m = Y.shape[0]
     Y_one_hot = np.zeros(shape=(classes, m))
     for i in range(m) :
-      Y_one_hot[Y[i]][i] = 1
+        Y_one_hot[Y[i]][i] = 1
 
     return Y_one_hot
