13fe4b605873d3c5b696276a845525cc1e8e0de3
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 15:54:24 +0100

    Update 22-deep_neural_network.py

--- supervised_learning/0x01-classification/22-deep_neural_network.py
+++ supervised_learning/0x01-classification/22-deep_neural_network.py
@@ -94,17 +94,18 @@ class DeepNeuralNetwork:
                 "b" + str(i)]-(alpha * db)
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Trains the deep neural network"""
         if not isinstance(iterations, int):
-            raise TypeError ("iterations must be an integer")
+            raise TypeError("iterations must be an integer")
         if (iterations < 0):
             raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
-            raise TypeError ("alpha must be a float")
+            raise TypeError("alpha must be a float")
         if (alpha < 0):
-            raise ValueError("alpha must be positive ")
+            raise ValueError("alpha must be positive")
         m = Y.shape[1]
 
         for i in range(iterations):
             A, self.__cache = self.forward_prop(X)
             self.gradient_descent(Y, self.__cache, alpha)
-        return self.evaluate(X,Y)
+        return self.evaluate(X, Y)
