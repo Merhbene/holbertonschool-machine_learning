71459af7f6f0977acfd275cf21257a0c553b5a60
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 14:29:20 +0100

    Update 15-neural_network.py

--- supervised_learning/0x01-classification/15-neural_network.py
+++ supervised_learning/0x01-classification/15-neural_network.py
@@ -123,11 +123,11 @@ class NeuralNetwork:
         for i in range(iterations + 1):
             self.__A1, self.__A2 = self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
-            c = self.cost(X, self.__A2)
+            a, cost = self.evaluate(X, Y)
             if verbose is True:
                   if (i % step == 0) or (i == iterations):
-                      print("Cost after {} iterations: {}".format(i, c))
-                      Cost.append(c)
+                      print("Cost after {} iterations: {}".format(i, cost))
+                      Cost.append(cost)
                       Iteration.append(i)
         if graph is True:
             plt.plot(Iteration, Cost, 'b')
@@ -136,5 +136,4 @@ class NeuralNetwork:
             plt.ylabel('cost')
             plt.show()
         return self.evaluate(X, Y)
-      
-      
+
