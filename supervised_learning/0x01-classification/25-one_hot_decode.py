efe84e6ebd898728364662940308a7cfc65be019
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 16:16:48 +0100

    Update 25-one_hot_decode.py

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+"""one hor decode function"""
+import numpy as np
+
 
 def one_hot_decode(one_hot):
-   classes, m=one_hot.shape
-   Y_decoded=np.zeros(shape=(m,))
+   try:
+         classes, m = one_hot.shape
+         Y_decoded = np.zeros(shape = (m,))
 
-   for i in range(m):
-     for j in range(classes):
-       if one_hot[j][i]==1 :
-         Y_decoded[i]=j
-   return Y_decoded.astype(int)
-   
+         for i in range(m):
+              for j in range(classes):
+                   if one_hot[j][i] == 1 :
+                        Y_decoded[i] = j
+         return Y_decoded.astype(int)
+   exept Exception :
+      return None
