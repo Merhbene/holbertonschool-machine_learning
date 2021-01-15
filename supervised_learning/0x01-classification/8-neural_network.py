7d9a683a0228bb0ac56950c57406254a97b09338
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 11:08:31 +0100

    Update 8-neural_network.py

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
+""" NeuralNetwork class """
+import numpy as np
+
 
 class NeuralNetwork:
-  def __init__(self, nx, nodes):
-    self.nx=nx
-    self.nodes=nodes
+    """A neural network with one hidden layer"""
+    def __init__(self, nx, nodes):
 
-    if type(self.nx)!=int:
-      raise Exception("nx must be an integer")
-    if self.nx<1 :
-      raise Exception("nx must be a positive integer")
-    if type(self.nodes)!=int:
-      raise Exception("nx must be an integer")
-    if self.nodes<1 :
-      raise Exception("nx must be a positive integer")
+        self.nx = nx
+        self.nodes = nodes
 
-    self.W1=np.random.normal(size=(self.nodes, self.nx))
-    self.b1=np.zeros(shape=(self.nodes,1))
-    self.A1=0
-    self.W2=np.random.normal(size=(1,self.nodes))
-    self.b2=0
-    self.A2=0
+        if type(self.nx) != int:
+            raise TypeError("nx must be an integer")
+        if self.nx < 1 :
+            raise ValueError("nx must be a positive integer")
+        if type(self.nodes) != int:
+            raise TypeError("nx must be an integer")
+        if self.nodes < 1 :
+            raise ValueError("nx must be a positive integer")
 
+        """Public instance attributes"""
+        self.W1 = np.random.normal(size=(self.nodes, self.nx))
+        self.b1 = np.zeros(shape=(self.nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, self.nodes))
+        self.b2 = 0
+        self.A2 = 0
