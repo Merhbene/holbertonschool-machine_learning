7f5a2b5ebbaf222b76282b51a6827db167326521
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 16:23:27 +0100

    Update 25-one_hot_decode.py

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -8,11 +8,11 @@ def one_hot_decode(one_hot):
     try:
        classes, m = one_hot.shape
        "classes is the maximum number of classes"
-       Y_decoded = np.zeros(shape = (m,))
+       Y_decoded = np.zeros(shape=(m,))
        for i in range(m):
            for j in range(classes):
-                if one_hot[j][i] == 1 :
-                    Y_decoded[i] = j
+              if one_hot[j][i] == 1:
+                  Y_decoded[i] = j
        return Y_decoded.astype(int)
     except Exception:
        return None
