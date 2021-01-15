4ad3dccfcf0382342ea8a4a9c684599176333a82
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-15 13:58:32 +0100

    Update 13-neural_network.py

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -85,17 +85,16 @@ class NeuralNetwork:
         """Calculate one pass of gradient descent on the neural network"""
         m = Y.shape[1]
         dz2 = A2 - Y
+        dz1 = np.matmul(self.__W2.T, dz2) * A1 * (1 - A1)
+
         dw2 = (1 / m)*np.matmul(dz2, A1.T)
         db2 = (1 / m)*np.sum(dz2, axis=1, keepdims=True)
+
+        dw1 = (1 / m)*np.matmul(dz1, X.T)
+        db1 = (1 / m)*np.sum(dz1, axis=1, keepdims=True)
 
         """Updates"""
         self.__W2 = self.__W2 - (alpha * dw2)
         self.__b2 = self.__b2 - (alpha * db2)
-        dz1 = np.dot(self.__W2.T, A2 - Y) * A1 * (1 - A1)
-
-        #dz1 = np.dot(self.__W2.T, dz2) * A1 * (1 - A1)
-        dw1 = (1 / m)*np.matmul(dz1, X.T)
-        db1 = (1 / m)*np.sum(dz1, axis=1, keepdims=True)
-
         self.__W1 = self.__W1 - (alpha * dw1)
         self.__b1 = self.__b1 - (alpha * db1)
