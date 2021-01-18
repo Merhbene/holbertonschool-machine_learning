74191a73590693ae66bcb1b015976ad2fe1953b6
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-18 20:43:52 +0100

    Create 0-create_confusion.py

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+     """labels is a one-hot numpy.ndarray of shape (m, classes)
+     containing the correct labels for each data point
+     """
+     m, classes = labels.shape
+     confusion_matrix = np.zeros((classes,classes))
+
+     for c in range (classes):
+        for line in range (m):
+            if logits[line][c] == 1:
+                for col in range (classes):
+                    confusion_matrix[col][c] = confusion_matrix[col][c]+labels[line][col]
+
+     return confusion_matrix
