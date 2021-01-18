109364ba886eb869f605d8c798f33a34c459b903
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-18 21:41:22 +0100

    Update 1-sensitivity.py

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 def sensitivity(confusion):
-    """"Sensitivity measures the proportion of 
+    """"
+    Sensitivity measures the proportion of
     positives that are correctly identified
     """
     classes = confusion.shape[0]
