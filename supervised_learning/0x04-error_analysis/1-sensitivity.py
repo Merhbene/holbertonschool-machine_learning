81c938d01f9f58645560d4287ec65958e51fa341
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-18 21:39:44 +0100

    Update 1-sensitivity.py

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -4,14 +4,13 @@ import numpy as np
 
 
 def sensitivity(confusion):
+    """"Sensitivity measures the proportion of 
+    positives that are correctly identified"""
     classes = confusion.shape[0]
     sensitivity = np.zeros((classes,))
     for j in range(classes):
         TP = confusion[j][j]
-        P=np.sum(confusion,axis=1)[j]
+        P = np.sum(confusion, axis=1)[j]
         sensitivity[j] = TP / P
 
     return sensitivity
-
-    
-
