9774819eab70c4250eae4a67e2f7b92f55353e85
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-19 10:12:08 +0100

    Update 4-f1_score.py

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -6,10 +6,15 @@ import numpy as np
 def f1_score(confusion):
     classes = confusion.shape[0]
     f1_score = np.zeros((classes,))
+    PREC = precision(confusion)
+    REC = sensitivity(confusion)
+    f1_score = (2 * PREC * REC) / (PREC + REC)
+    """
     for j in range(classes):
         TP = confusion[j][j]
         FP = np.sum(np.delete(confusion, [j],axis=0), axis=0)[j]
         FN = np.sum(np.delete(confusion, [j],axis=1), axis=1)[j]
         f1_score[j] = TP / (TP + 0.5 * (FP + FN))
-
-    return f1_score   
+    """
+    
+    return f1_score
