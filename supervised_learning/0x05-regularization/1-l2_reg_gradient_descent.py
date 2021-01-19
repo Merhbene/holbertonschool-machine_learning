68668e35eb189f2342b12d4d685b270822b585b0
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-19 15:53:01 +0100

    Update 1-l2_reg_gradient_descent.py

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -10,7 +10,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     for i in range(L, 0, -1):
         dw = (1 / m)*(np.matmul(dz, cache["A" + str(i-1)].T) + (lambtha * weights["W" + str(i)]))
         db = (1 / m)*np.sum(dz, axis=1, keepdims=True)
-        dA = cache["A" + str(i-1)]*(1 - cache["A"+str(i-1)])
+        dA = (1 - cache["A"+str(i-1)] ** 2)
         dz = np.matmul(weights["W" + str(i)].T, dz) * dA
         weights["W" + str(i)] = weights["W" + str(i)] - (alpha * dw)
         weights["b" + str(i)] = weights["b" + str(i)]-(alpha * db)
