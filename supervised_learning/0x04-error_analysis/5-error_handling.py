2a432b4176b5f8779893e762db083fbe33fd0399
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-19 11:00:26 +0100

    Create 5-error_handling.py

---
+++ supervised_learning/0x04-error_analysis/5-error_handling.py
@@ -0,0 +1,4 @@
+1. High Bias, High Variance: B. Try a different architecture 
+2. High Bias, Low Variance: A. Train more D. Build a deeper network
+3. Low Bias, High Variance: C. Get more data E. Use regularization
+4. Low Bias, Low Variance: F. Nothing
