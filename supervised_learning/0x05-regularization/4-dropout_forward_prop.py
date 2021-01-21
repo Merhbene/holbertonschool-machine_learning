0fd68fbca4ff0b025254678063e363b92dd7ef56
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-21 15:50:36 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -6,16 +6,16 @@ import numpy as np
 def dropout_forward_prop(X, weights, L, keep_prob):
     "conduct forward propagation using Dropout"
     m = X.shape[1]
-    D={}
+    D = {}
     D["A0"] = X
     for i in range(L+1):
        if i == L:
-          z = np.matmul(weights["W" + str(i)], D["A" + str(i-1)]) + weights["b" + str(i)]
-          A = (np.exp(z) / np.sum(np.exp(z), axis=0, keepdims=True))
-          D["A" + str(i)] = A
+           z = np.matmul(weights["W" + str(i)], D["A" + str(i-1)]) + weights["b" + str(i)]
+           A = (np.exp(z) / np.sum(np.exp(z), axis=0, keepdims=True))
+           D["A" + str(i)] = A
        else:
-          z = np.matmul(weights["W" + str(i+1)], D["A" + str(i)]) + weights["b" + str(i+1)]
-          A = np.tanh(z)
-          D["D" + str(i+1)] = np.random.binomial(1, keep_prob, A.shape)
-          D["A" + str(i+1)] = (A * D["D" + str(i+1)]) / keep_prob
+           z = np.matmul(weights["W" + str(i+1)], D["A" + str(i)]) + weights["b" + str(i+1)]
+           A = np.tanh(z)
+           D["D" + str(i+1)] = np.random.binomial(1, keep_prob, A.shape)
+           D["A" + str(i+1)] = (A * D["D" + str(i+1)]) / keep_prob
     return D
