5a31e87a4e96de20ac2fb692abb543c99aab2045
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-21 16:04:00 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -1,27 +1,25 @@
+
 #!/usr/bin/env python3
 "A Dropout function"
 import numpy as np
 
 
-def dropout_forward_prop(X, weights, L, keep_prob):
+def dropout_forward_prop(X, weights, L, keep_prob): 
     "conduct forward propagation using Dropout"
-    cache = {'A0': X}
+    m = X.shape[1]
+    D={}
+    D["A0"] = X 
 
-    for i in range(L):
-        W_key = "W{}".format(i + 1)
-        b_key = "b{}".format(i + 1)
-        A_key_prev = "A{}".format(i)
-        A_key_forw = "A{}".format(i + 1)
-        D_key = "D{}".format(i + 1)
+    for i in range(1, L):
+       z = np.matmul(weights["W" + str(i)], D["A" + str(i-1)]) + weights["b" + str(i)]
 
-        Z = np.matmul(weights[W_key], cache[A_key_prev]) + weights[b_key]
-        drop = np.random.binomial(1, keep_prob, size=Z.shape)
+       if i == L-1 :
+          A = (np.exp(z) / np.sum(np.exp(z), axis=0, keepdims=True))#softmax
+          D["A" + str(i+1)] = A
+       else:
 
-        if i == L - 1:
-            t = np.exp(Z)
-            cache[A_key_forw] = (t / np.sum(t, axis=0, keepdims=True))
-        else:
-            cache[A_key_forw] = np.tanh(Z)
-            cache[D_key] = drop
-            cache[A_key_forw] = (cache[A_key_forw] * cache[D_key]) / keep_prob
-    return cache
+          A = np.tanh(z) 
+          D["D" + str(i)] = np.random.binomial(1, keep_prob, A.shape)
+          D["A" + str(i)] = (A * D["D" + str(i)]) / keep_prob 
+
+    return D
