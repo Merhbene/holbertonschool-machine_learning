a5ba71538167a4b091c532aed6bb810ebeb29563
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-21 14:02:08 +0100

    Create 4-dropout_forward_prop.py

---
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"A Dropout function"
+import tensorflow as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob): 
+    "conduct forward propagation using Dropout"
+    m = X.shape[1]
+    D={}
+    for i in range(L + 1):
+       if i == 0:
+          D["A0"] = X
+       else:
+          z = np.matmul(weights["W" + str(i)], 
+                        D["A" + str(i-1)]) + weights["b" + str(i)]
+          D["A" + str(i)] = np.tanh(z) 
+    
+       D["D" + str(i+1)] = np.random.binomial(1, 0.8, D["A" + str(i)].shape)
+
+    return D
