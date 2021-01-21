9be0b105d2f9ea65f30f6916d95e48908fd84304
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-21 12:55:42 +0100

    Update 3-l2_reg_create_layer.py

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -8,5 +8,5 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     "create a tensorflow layer that includes L2 regularization"
     k = tf.contrib.layers.l2_regularizer(lambtha)
     initial = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.keras.layers.Dense(n, activation=activation, kernel_regularizer=k, kernel_initializer=initial)
-    return (layer(prev))
+    layer = tf.keras.layers.Dense(n, activation=activation, kernel_initializer=initial, kernel_regularizer=k)
+    return layer(prev)
