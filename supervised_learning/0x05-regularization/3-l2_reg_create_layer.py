262eea4b4830ddc2cd594bb422a83928c20727c0
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-21 12:10:38 +0100

    Create 3-l2_reg_create_layer.py

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"l2 regularization function "
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    "create a tensorflow layer that includes L2 regularization"
+    k = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.keras.layers.Dense(n, activation=activation, kernel_regularizer=k)
+    return layer
