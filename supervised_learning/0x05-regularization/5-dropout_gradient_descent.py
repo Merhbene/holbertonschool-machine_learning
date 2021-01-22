313439efeb685e0c3e9640d4e8deeea562d7ee3a
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-22 20:19:40 +0100

    Update 5-dropout_gradient_descent.py

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -11,7 +11,8 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         db = (1 / m)*np.sum(dz, axis=1, keepdims=True)
         dA = (1 - cache["A"+str(i-1)] ** 2)
         dz = np.matmul(weights["W" + str(i)].T, dz) * dA
-        weights["W" + str(i)] = weights["W" + str(i)] - (alpha * dw)
-        weights["b" + str(i)] = weights["b" + str(i)]-(alpha * db)
         if i>1:
             dz = np.matmul(weights["W" + str(i)].T, dz) * dA * cache["D"+str(i-1)] / keep_prob
+        weights["W" + str(i)] = weights["W" + str(i)] - (alpha * dw)
+        weights["b" + str(i)] = weights["b" + str(i)]-(alpha * db)
+
