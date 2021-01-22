a7e4321750a041f82ee08d3d79fdfe70f0a1fd49
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-22 08:56:51 +0100

    Update 0-l2_reg_cost.py

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"calculate the cost of a neural network with L2 regularization"
 import numpy as np
+"calculate the cost of a neural network with L2 regularization"
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     reg = 0
-    "cost is the cost of the network without L2 regularization"
+    "cost: the cost of the network without L2 regularization"
     for i in range(1, L+1):
         w = weights['W' + str(i)]
         reg += np.linalg.norm(w)
