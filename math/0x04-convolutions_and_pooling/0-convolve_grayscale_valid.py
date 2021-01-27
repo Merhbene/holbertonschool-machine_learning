528cd77a53df4da7d9afc4bfb6c653701d6fc2e6
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-27 11:54:56 +0100

    Update 0-convolve_grayscale_valid.py

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -4,7 +4,6 @@ import numpy as np
 
 def convolve_grayscale_valid(images, kernel):
 
-
     m, h, w = images.shape
     kh, kw = kernel.shape
 
@@ -12,20 +11,18 @@ def convolve_grayscale_valid(images, kernel):
     o_h = h - kh + 1
     o_w = w - kw + 1
 
-
-
     # creating outputs of size: n_images, o_h x o_w
     outputs = np.zeros((m, o_h, o_w))
 
     # vectorizing the n_images
-    images_array = np.arange(0, m)
+    image = np.arange(0, m)
 
     # iterating over the output array and generating the convolution
     for x in range(o_h):
         for y in range(o_w):
             x1 = x + kh
             y1 = y + kw
-            outputs[images_array, x, y] = np.sum(np.multiply(
-                images[images_array, x: x1, y: y1], kernel), axis=(1, 2))
+            outputs[image, x, y] = np.sum(np.multiply(
+                images[image, x: x1, y: y1], kernel), axis=(1, 2))
 
     return outputs
