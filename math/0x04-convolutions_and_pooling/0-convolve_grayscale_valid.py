a5e4b5bcaf81395fb7a94e13bf964a788149ec56
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-27 14:26:39 +0100

    Update 0-convolve_grayscale_valid.py

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -7,24 +7,24 @@ def convolve_grayscale_valid(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # output shape 
-    output_dim = (m, h - kh + 1, w - kw +1)
+    # output shape
+    output_dim = (m, h - kh + 1, w - kw + 1)
 
     # creating outputs 
     outputs = np.zeros(output_dim)
 
     # vectorizing the m images
-    #image = np.arange(0, m)
+    # image = np.arange(0, m)
 
     # iterating over the output array and generating the convolution
     for i in range(output_dim[1]):
         for j in range(output_dim[2]):
             x = i + kh
             y = j + kw
-            #outputs[image, i, j] = np.sum(np.multiply(images[image, i: x, j: y], kernel), axis=(1, 2)) "marche"
-            #outputs[image, i, j] = (images[image, i:x, j:y] * kernel).sum() "ne marche pas"
+            # outputs[image, i, j] = np.sum(np.multiply(images[image, i: x, j: y], kernel), axis=(1, 2)) "marche"
+            # outputs[image, i, j] = (images[image, i:x, j:y] * kernel).sum() "ne marche pas"
 
-            M = images[:, i:x, j:y] 
-            outputs[:,i, j] = np.tensordot(M, kernel)
+            M = images[:, i:x, j:y]
+            outputs[:, i, j] = np.tensordot(M, kernel)
 
     return outputs
