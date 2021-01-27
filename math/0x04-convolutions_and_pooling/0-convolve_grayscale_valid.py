14745b4708065ee4e481a369955af486c7613fa9
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-27 14:24:24 +0100

    Update 0-convolve_grayscale_valid.py

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -14,7 +14,7 @@ def convolve_grayscale_valid(images, kernel):
     outputs = np.zeros(output_dim)
 
     # vectorizing the m images
-    image = np.arange(0, m)
+    #image = np.arange(0, m)
 
     # iterating over the output array and generating the convolution
     for i in range(output_dim[1]):
@@ -22,9 +22,9 @@ def convolve_grayscale_valid(images, kernel):
             x = i + kh
             y = j + kw
             #outputs[image, i, j] = np.sum(np.multiply(images[image, i: x, j: y], kernel), axis=(1, 2)) "marche"
-            outputs[image, i, j] = (images[image, i:x, j:y] * kernel).sum(axis=(1, 2)) "ne marche pas"
-            """
-            M = images[:,i:x, j:y] 
+            #outputs[image, i, j] = (images[image, i:x, j:y] * kernel).sum() "ne marche pas"
+
+            M = images[:, i:x, j:y] 
             outputs[:,i, j] = np.tensordot(M, kernel)
-            """
+
     return outputs
