d775ce775aa9a134a82af399e2e74e11f95d81dc
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-01-29 17:58:34 +0100

    Update 3-convolve_grayscale.py

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,7 +11,7 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
        ph = pw = 0
 
     elif padding == 'same':
-       ph = int((((h - 1) * sh + kh - h) / 2) +1 )
+       ph = int((((h - 1) * sh + kh - h) / 2) +1)
        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
 
     else: 
