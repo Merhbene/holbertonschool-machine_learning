b7a7d37760891a900a0a912cdc47bf446c78d5ad
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-02-04 12:10:20 +0100

    Update 0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -15,7 +15,6 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
        ph = int((((h_prev - 1) * sh + kh - h_prev) / 2) + (kh % 2 ==0))
        pw = int((((w_prev - 1) * sw + kw - w_prev) / 2) + (kh % 2 ==0))
 
-
     oh = int(((h_prev + 2 * ph - kh) / sh) + 1)
     ow = int(((w_prev + 2 * pw - kw) / sw) + 1)
 
@@ -23,7 +22,9 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
     outputs = np.zeros(output_dim)
 
-    padded_images = np.pad(A_prev, pad_width=((0, 0),(ph, ph),(pw, pw),(0, 0)), mode='constant', constant_values=0)
+    padded_images = np.pad(A_prev, pad_width=((0, 0), (ph, ph),
+                                              (pw, pw),(0, 0)),
+                           mode='constant', constant_values=0)
 
     for i in range(oh):
         for j in range(ow):
@@ -32,7 +33,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
             M = padded_images[:, (i * sh):x, (j * sw):y, :]
             for k in range(c_new):
-               
+
               outputs[:, i, j, k] = np.tensordot(M, W[:,:,:,k], axes=3)# axes=3 puisqu'on fait une convolution sur 2 cubes(3D)
 
     A = activation(outputs + b)
