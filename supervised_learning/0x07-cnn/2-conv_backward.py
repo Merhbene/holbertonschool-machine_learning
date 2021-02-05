be58e22f0580c025b0d92ad898b95111ae977037
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-02-05 18:08:02 +0100

    Update 2-conv_backward.py

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -13,8 +13,8 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     ph = pw = 0
 
     if padding == 'same':
-        ph = int((((h_prev - 1) * sh + kh - h_prev) / 2) + 1)  # +(kh % 2 == 0)
-        pw = int((((w_prev - 1) * sw + kw - w_prev) / 2) + 1)
+       ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2 ))
+       pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2 ))
 
     A_prev = np.pad(A_prev, pad_width=((0, 0),
                     (ph, ph), (pw, pw), (0, 0)),
