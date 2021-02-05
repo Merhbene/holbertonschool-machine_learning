0e86c517192eaec4f887ca151bb24f9dbb93eb2c
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-02-05 18:30:56 +0100

    Update 3-pool_backward.py

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
    "perform back propagation over a pooling layer of a neural network"
-   m,h_new,w_new,c_new = dA.shape
+   m, h_new, w_new, c_new = dA.shape
    dA_prev = np.zeros(A_prev.shape)
    kh, kw = kernel_shape[0], kernel_shape[1]
    sh, sw = stride[0], stride[1]
@@ -17,7 +17,7 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
                 if mode == "max":
                     mask = (A_prev_mini == np.max(A_prev_mini))
                 else:
-                    average = 1 / (kh * kw )
+                    average = 1 / (kh * kw)
                     mask = average * np.ones(kernel_shape)
-                dA_prev[i,h*sh:h*sh+kh,w*sw:w*sw+kw,c] += mask * dA[i,h,w,c]
+                dA_prev[i, h* sh:h * sh + kh, w * sw:w * sw + kw, c] += mask * dA[i, h, w, c]
    return dA_prev     
