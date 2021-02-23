195d60694b4e5debafbd0ac48043bac6d61502b1
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-02-23 10:48:56 +0100

    Create 0-yolo.py

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Initialize Yolo_V3 """
+
+
+class Yolo:
+    """ YOLO_V3"""
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = K.models.load_model(model_path)
+        f = open(classes_path, "r")
+        self.class_names  = [c[:-1] for c in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
