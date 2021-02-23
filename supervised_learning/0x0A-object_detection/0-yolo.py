de59aca2d573fa41bcce90ff91f1204b528e7613
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-02-23 10:51:01 +0100

    Update 0-yolo.py

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -7,7 +7,7 @@ class Yolo:
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
         f = open(classes_path, "r")
-        self.class_names  = [c[:-1] for c in f]
+        self.class_names = [c[:-1] for c in f]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
