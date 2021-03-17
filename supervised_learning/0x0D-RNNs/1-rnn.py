a045f61a57b5c3c1a908805b6a7c3e46181c7ac0
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-03-17 12:40:20 +0100

    Create 1-rnn.py

---
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """Perform forward propagation for a simple RNN"""
+    t, _, _ = X.shape
+    H = [h_0]
+    Y = []
+    for step in range(t):
+          h, y = rnn_cell.forward(h_0, X[step])
+          H.append(h)
+          Y.append(y)
+    return np.array(H), np.array(Y)
