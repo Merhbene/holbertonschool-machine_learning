6d08a055e279f90432be55757114d560a33e8826
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-03-18 11:32:57 +0100

    Update 4-deep_rnn.py

--- supervised_learning/0x0D-RNNs/4-deep_rnn.py
+++ supervised_learning/0x0D-RNNs/4-deep_rnn.py
@@ -9,11 +9,11 @@ def deep_rnn(rnn_cells, X, h_0):
     for xt in X:
         ht = []
         M = xt
-        for i in range len(rnn_cells):
+        for i in range(len(rnn_cells)):
             h, y = rnn_cells[i].forward(H[-1][i], M)
             ht.append(h)
             M = h
-            if i == (len(rnn_cells)-1)
+            if i == (len(rnn_cells)-1):
                 Y.append(y)
         H.append(ht)
     return np.array(H), np.array(Y)
