9110be1d1be29f17cedc4d4cc16e146d27ff240b
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-04-01 15:35:40 +0100

    Create 6-howdy_partner.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains the function cat_arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """returns a new array that concatenates arr1 and arr2"""
+    return arr1 + arr2
