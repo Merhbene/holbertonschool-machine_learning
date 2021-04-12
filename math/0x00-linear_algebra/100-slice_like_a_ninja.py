95136914738fcec577af0076f08d0790bdf42885
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-04-12 17:24:06 +0200

    Update 100-slice_like_a_ninja.py

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -6,9 +6,29 @@ This module contains the function np_slice
 
 def np_slice(matrix, axes={}):
     """This returns a slice of an ndarray"""
-    last = -1
-    mat_slice = []
-    for axis in sorted(axes):
-        mat_slice += [slice(None)]* (axis - last - 1) + [slice(*axes[axis])]
-        last = axis
-    return matrix[mat_slice]
+    for a in sorted(axes.keys()):
+
+        val = axes[a]
+        if len(val) == 3:
+            start = val[0]
+            stop = val[1]
+            step = val[2]
+        elif len(val) == 2:
+            start = val[0]
+            stop = val[1]
+            step = None
+        elif len(val) == 1:
+            start = None
+            stop = val[0]
+            step = None
+        else:
+            start = None
+            stop = None
+            step = None
+
+        s = slice(start, stop, step)
+        slc = [slice(None)] * len(matrix.shape)
+        slc[a] = s
+        matrix = matrix[tuple(slc)]
+
+    return matrix
