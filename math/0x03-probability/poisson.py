8b6daff2418d81d27d5d44967b0a2cdd43f31a00
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-04-26 11:10:25 +0100

    Update poisson.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 "new class"
+
 
 class Poisson():
     "poisson distribution"
@@ -7,16 +8,16 @@ class Poisson():
 
         if data is None:
             self.lambtha = lambtha
-            if (lambtha<=0) :
+            if (lambtha<=0):
                 raise ValueError("lambtha must be a positive value")
          
-        if data:
+        if data is not None:
             #Calculate the lambtha of data
             if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data)<2 :
+            if (len(data)<2):
                 raise ValueError("data must contain multiple values")   
-            L=0
+            L = 0
             for i in data:
-                L = L+i
+                L = L + i
             self.lambtha = L / len(data)
