4512f0ee428e2e2c268ff5d368726aefee1013c5
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-04-26 13:22:26 +0100

    Create exponential.py

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"new class"
+
+
+class Exponential():
+    "exponential distribution"
+    def __init__(self, data=None, lambtha=1.):
+
+        if data is None:
+            self.lambtha = float(lambtha)
+            if (lambtha <= 0):
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if (len(data) < 2):
+                raise ValueError("data must contain multiple values")
+
+            # Calculate the lambtha of data
+            self.lambtha = len(data) / sum(data)
