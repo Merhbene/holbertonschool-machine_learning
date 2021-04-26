b76c408a410e52efc1ca7fe82b9f34b9536bd733
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-04-26 12:37:46 +0100

    Update poisson.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -2,38 +2,33 @@
 "new class"
 
 
-def factorial(x):
-    "factorial"
-    f = 1
-    for i in range(1, x + 1):
-        f *= i
-    return f
 class Poisson():
     "poisson distribution"
     def __init__(self, data=None, lambtha=1.):
 
         if data is None:
-            self.lambtha = float(lambtha)
+            self.lambtha = float (lambtha)
             if (lambtha <= 0):
                 raise ValueError("lambtha must be a positive value")
-
-        if data is not None:
-            # Calculate the lambtha of data
+        else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if (len(data) < 2):
                 raise ValueError("data must contain multiple values")
-            L = 0
-            for i in data:
-                L = L + i
-            self.lambtha = L / len(data)
+
+            # Calculate the lambtha of data
+            self.lambtha = sum(data) / len(data)
 
     # probability mass function
     def pmf(self, k):
         "Calculates the value of the PMF for a given number of “successes” "
-        k = int(k)
+        k = int (k)
         if k < 0:
             return 0
+
         e = 2.7182818285
-        P = ((self.lambtha ** k) * (e ** (- self.lambtha))) / factorial(k)
+        fact_k = 1
+        for i in range(1, k + 1):
+            fact_k *= i
+        P = ((self.lambtha ** k) * (e ** (- self.lambtha))) / fact_k
         return P
