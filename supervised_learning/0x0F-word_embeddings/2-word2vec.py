55739b4aa5c54be575ad28cb2ffb3643722f4ef8
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-17 13:40:28 +0100

    Update 2-word2vec.py

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -8,7 +8,7 @@ def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
     "create and trains a gensim word2vec model"
 
     model = Word2Vec(sentences=sentences, size=size, min_count=min_count,
-                                   window=window, negative=negative, sg=cbow, seed=seed,
+                                   window=window, negative=negative, sg=not cbow, seed=seed,
                                    iter=iterations, workers=workers)
     model.train(sentences, total_examples=model.corpus_count, epochs=model.iter)  # train word vectors
     return model
