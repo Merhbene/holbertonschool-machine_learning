88ce9040a1bc0f110efd23f04e55fc0df4705b93
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-17 13:05:21 +0100

    Create 0-norm_constants.py

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Word Embeddings module"""
+from gensim.models import Word2Vec
+
+
+def word2vec_model(
+    sentences,
+    size=100,
+    min_count=5,
+    window=5,
+    negative=5,
+    cbow=True,
+    iterations=5,
+    seed=0,
+    workers=1
+):
+    """Creates and trains a Word2Vec model
+    Arguments:
+        sentences {list[str]} -- Is a list of strings
+    Keyword Arguments:
+        size {int} -- Dimensinality of embedding layer (default: {100})
+        min_count {int} -- Minimum number of occurences (default: {5})
+        window {int} -- Maximum distance (default: {5})
+        negative {int} -- Is the size of negative sampling (default: {5})
+        cbow {bool} -- Indicates CBOW or Skip-gram (default: {True})
+        iterations {int} -- Is the number of iterations (default: {5})
+        seed {int} -- Is the seed for the random number (default: {0})
+        workers {int} -- Is the number of workers (default: {1})
+    Returns:
+        model -- Trained model
+    """
+    return Word2Vec(
+        sentences,
+        size=size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        iter=iterations,
+        workers=workers,
+        seed=seed,
+        sg=not cbow
+    )
