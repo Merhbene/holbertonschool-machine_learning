91dd8568d9193d69848ae85bf2650aadb97da5f7
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-19 09:55:19 +0100

    Update 3-gensim_to_keras.py

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -4,6 +4,6 @@
 
 def gensim_to_keras(model):
     "converts a gensim word2vec model to a keras Embedding layer"
-    model.wv.get_keras_embedding(train_embeddings=False)
+    model.wv.get_keras_embedding(train_embeddings=True)
 
     return model
