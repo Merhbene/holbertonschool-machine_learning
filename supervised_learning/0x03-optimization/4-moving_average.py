02836e600a2d57e981a8496a250e41f09d7f5209
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-19 12:15:37 +0100

    Update 4-moving_average.py

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -4,8 +4,8 @@
 
 def moving_average(data, beta):
     "calculates the weighted moving average of a data set"
-    v=0
-    Avg=[]
+    v = 0
+    Avg = []
     for i, d in enumerate(data):
         v = beta * v + (1 - beta) * d
         bias_corr = v / (1 - (beta ** (i + 1)))
