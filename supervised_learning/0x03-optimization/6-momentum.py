3ac1347b5acd61848ba0a7b7644b4fa6d51e0d3e
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-19 15:04:52 +0100

    Update 6-momentum.py

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -8,8 +8,8 @@ def create_momentum_op(loss, alpha, beta1):
     "create the training operation for a neural network in tensorflow"
     optimizer = tf.train.MomentumOptimizer(alpha, beta1)
 
-    """ Add operations to minimize loss by updating var_list, 
+    """ Add operations to minimize loss by updating var_list,
      This method simply combines calls compute_gradients() and
      apply_gradients()"""
-    
+
     return optimizer.minimize(loss)
