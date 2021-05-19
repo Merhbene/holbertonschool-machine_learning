3a9f1130310b07fa854e488c233e90341b27337d
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-19 14:37:37 +0100

    Update 5-momentum.py

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -3,7 +3,8 @@
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-    "update a variable using the gradient descent with momentum optimization algorithm"
+    """update a variable using the gradient descent with momentum
+    optimization algorithm"""
     v_t = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v_t
 
