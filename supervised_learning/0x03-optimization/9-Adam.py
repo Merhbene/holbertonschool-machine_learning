9b995afaa65f23b4af05d1d4228c0e6c753584a8
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-19 16:43:36 +0100

    Update 9-Adam.py

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -5,11 +5,12 @@ import numpy as np
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """update a variable in place using the Adam optimization algorithm"""
-
     v = beta1 * v + (1 - beta1) * grad
-    v_c = v / (1 - beta1 ** t)
     s = beta2 * s + (1 - beta2) * (grad ** 2)
-    s_c = s / (1 - beta2 ** t)
-    var -= alpha * v_c / (np.sqrt(s_c) + epsilon)
+
+    v_t  = v / (1 - beta1 ** t)
+    s_t = s / (1 - beta2 ** t)
+
+    var = var - ((alpha * v_t) / (np.sqrt(s_t) + epsilon))
 
     return var, v, s
