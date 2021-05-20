2f4d6bc0c0500d19641f7dc9b5220dcc34b6ae33
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-20 11:31:28 +0100

    Update 14-batch_norm.py

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -3,14 +3,13 @@
 import tensorflow as tf
 
 
-def create_batch_norm_layer(prev, n, activation): 
+def create_batch_norm_layer(prev, n, activation):
     """that creates a batch normalization layer for a
      neural network in tensorflow"""
 
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(n, kernel_initializer=init)(prev)
 
-    
     gamma = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma', trainable=True)
     beta = tf.Variable(tf.constant(0.0, shape=[n]), name='beta', trainable=True)
 
