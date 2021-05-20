3a8dd289db0ce0803c11af31c2a9ef149b245214
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-20 11:10:20 +0100

    Update 13-batch_norm.py

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
-    """normalize an unactivated output of a neural 
+    """normalize an unactivated output of a neural
     network using batch normalization"""
     m = np.mean(Z, axis=0)
     v = np.var(Z, axis=0)
