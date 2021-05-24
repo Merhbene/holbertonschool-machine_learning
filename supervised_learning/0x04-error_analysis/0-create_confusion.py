06119680b194a790a3d8547f59d3f02a2efb8d1f
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-24 16:14:28 +0100

    Update 0-create_confusion.py

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -5,4 +5,10 @@ import numpy as np
 
 def create_confusion_matrix(labels, logits):
     """creates a confusion matrix from labels and logits"""
-    return np.matmul(labels.T, logits)
+    m, classes = labels.shape
+    confusion = np.zeros((classes, classes))
+    labels = np.argmax(labels, axis=1)
+    logits = np.argmax(logits, axis=1)
+    for i in range(m):
+        confusion[labels[i], logits[i]] += 1
+    return confusion
