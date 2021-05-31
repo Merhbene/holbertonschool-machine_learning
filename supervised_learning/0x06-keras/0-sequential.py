1a286fb5b2d9b25741b0cd84bb06d6f23768f239
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-05-31 11:32:10 +0100

    Create 0-sequential.py

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"Sequential"
+import tensorflow.keras as k
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    "builds a neural network with the Keras library"
+    reg = k.regularizers.l2(l=lambtha)
+    model = k.Sequential()
+    model.add(k.layers.Dense(layers[0], input_shape=(nx,), activation=activations[0], kernel_regularizer=reg))
+    for i in range(1, len(layers)):
+        model.add(k.layers.Dropout(rate=keep_prob)) #Fraction of the input units to drop
+        model.add(k.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=reg))
+
+    return model
