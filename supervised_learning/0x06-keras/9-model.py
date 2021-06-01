a337c966e806530e83177d98883bf95198af5793
Merhbene <merhbeno@gmail.com>
2021-06-01 16:29:22 +0100

    save/load

---
+++ supervised_learning/0x06-keras/9-model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    "saves a model’s weights"
+    network.save(filename)
+
+
+def load_model(filename):
+    "loads a model’s weights"
+    return K.models.load_model(filename)
