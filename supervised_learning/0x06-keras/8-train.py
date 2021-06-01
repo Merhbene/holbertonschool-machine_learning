c82a1a73908528cc5618587308dc817147113f14
Merhbene <merhbeno@gmail.com>
2021-06-01 16:09:23 +0100

    Save

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+" Save Only the Best"
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    "save the best iteration of the model:"
+    cb = []
+    if validation_data is not None:
+        if early_stopping:
+            early = K.callbacks.EarlyStopping(monitor='val_loss',
+                                              patience=patience)
+            cb.append(early)
+        if learning_rate_decay:
+            def decayed_lr(step):
+                """ Inverse time decay schedule function, step == epoch"""
+                return alpha / (1 + decay_rate * (step))
+            learn = K.callbacks.LearningRateScheduler(schedule=decayed_lr,
+                                                      verbose=True)
+            cb.append(learn)
+
+        if save_best:
+            saver = K.callbacks.ModelCheckpoint(filepath, save_best_only=True)
+            cb.append(saver)
+            
+    history = network.fit(data, labels, epochs=epochs,
+                          batch_size=batch_size,
+                          callbacks=cb,
+                          validation_data=validation_data,
+                          verbose=verbose, shuffle=shuffle)
+    return history
