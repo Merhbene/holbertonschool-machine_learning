bae2a71449c74a66de37796f8e32ca3673c415b5
Merhbene <merhbeno@gmail.com>
2021-06-01 16:11:23 +0100

    save

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -3,10 +3,13 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, early_stopping=False,
-                patience=0, learning_rate_decay=False, alpha=0.1,
-                decay_rate=1, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                learning_rate_decay=False,
+                alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None,
+                verbose=True, shuffle=False):decay_rate=1, verbose=True, shuffle=False):
     "save the best iteration of the model:"
     cb = []
     if validation_data is not None:
@@ -25,7 +28,7 @@ def train_model(network, data, labels, batch_size, epochs,
         if save_best:
             saver = K.callbacks.ModelCheckpoint(filepath, save_best_only=True)
             cb.append(saver)
-            
+
     history = network.fit(data, labels, epochs=epochs,
                           batch_size=batch_size,
                           callbacks=cb,
