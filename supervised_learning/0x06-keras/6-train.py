143269555f660916bdc5184b5ae4fbdbac92a60a
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2021-06-01 09:15:45 +0100

    Update 6-train.py

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -5,8 +5,9 @@ import tensorflow.keras as k
 
 def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, verbose=True, shuffle=False):
     "Stop training when a monitored quantity has stopped improving"
+    es = None
     if validation_data is not None and early_stopping:
-        es = k.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+        es = [k.callbacks.EarlyStopping(monitor='val_loss', patience=patience)]
     return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
-                       validation_data=validation_data, callbacks=[es],
+                       validation_data=validation_data, callbacks=es,
                        verbose=verbose, shuffle=shuffle)
