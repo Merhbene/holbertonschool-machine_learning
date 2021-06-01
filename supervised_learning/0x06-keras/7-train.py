fec43712ca0384eddd7064e5e7c1537057c8d599
Merhbene <merhbeno@gmail.com>
2021-06-01 15:56:09 +0100

    test

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -3,18 +3,29 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, verbose=True, shuffle=False):
-    "train the model with learning rate decay"
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
 
-    callbacks = None
-    if validation_data is not None and (early_stopping or learning_rate_decay):
-        callbacks = [] 
+    callbacks = []
+    if validation_data:
         if early_stopping:
-            callbacks.append(K.callbacks.EarlyStopping(monitor='val_loss', patience=patience))
+            early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                                   patience=patience)
+            callbacks.append(early_stop)
         if learning_rate_decay:
-            def schedule(epoch):
-                return alpha / (1 + decay_rate * epoch)
-            callbacks.append(K.callbacks.LearningRateScheduler(schedule=schedule, verbose=1))
-    return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
-                       validation_data=validation_data, callbacks=callbacks,
-                       verbose=verbose, shuffle=shuffle)
+            def schedule(step):
+                '''stepwise inverse time decay function'''
+                return alpha * 1 / (1 + decay_rate * step)
+            lr_decay = K.callbacks.LearningRateScheduler(schedule=schedule,
+                                                         verbose=1)
+            callbacks.append(lr_decay)
+    return network.fit(x=data,
+                       y=labels,
+                       batch_size=batch_size,
+                       validation_data=validation_data,
+                       epochs=epochs,
+                       verbose=verbose,
+                       callbacks=callbacks,
+                       shuffle=shuffle)
