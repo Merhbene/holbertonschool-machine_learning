e69e8c93c885b20a89e7a91cbb8c570ab5ff5496
Merhbene <merhbeno@gmail.com>
2021-10-25 12:08:36 +0100

    cov

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -3,12 +3,13 @@
 Mean and Covariance
 """
 
+
 import numpy as np
 
 def mean_cov(X):
     """calculates the mean and covariance of X"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
-            raise TypeError('X must be a 2D numpy.ndarray')
+        raise TypeError('X must be a 2D numpy.ndarray')
     n = X.shape[0]
     if n < 2:
         raise ValueError('X must contain multiple data points')
@@ -21,7 +22,8 @@ if __name__ == '__main__':
     mean_cov = __import__('0-mean_cov').mean_cov
 
     np.random.seed(0)
-    X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000)
+    X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15],
+                             [-30, 100, -20], [15, -20, 25]], 10000)
     mean, cov = mean_cov(X)
     print(mean)
     print(cov)
