05e0998d3eb88bd1649ec2ab011fa44317e75a6e
Merhbene <merhbeno@gmail.com>
2021-10-25 12:11:58 +0100

    test2

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -6,6 +6,7 @@ Mean and Covariance
 
 import numpy as np
 
+
 def mean_cov(X):
     """calculates the mean and covariance of X"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
@@ -17,13 +18,14 @@ def mean_cov(X):
     cov = np.matmul((X - mean).T, (X - mean)) / (n - 1)
     return mean, cov
 
+
 if __name__ == '__main__':
     import numpy as np
     mean_cov = __import__('0-mean_cov').mean_cov
 
     np.random.seed(0)
     X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15],
-                             [-30, 100, -20], [15, -20, 25]], 10000)
+                         [-30, 100, -20], [15, -20, 25]], 10000)
     mean, cov = mean_cov(X)
     print(mean)
     print(cov)
