c8deaddc671f276daf641ed99b800ef78a6ffbc6
Merhbene <merhbeno@gmail.com>
2021-10-28 16:12:21 +0100

    entropy

--- unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -10,14 +10,3 @@ def HP(Di, beta):
     Hi = np.sum(-Pi * np.log2(Pi))
     return Hi, Pi
 
-
-pca = __import__('1-pca').pca
-P_init = __import__('2-P_init').P_init
-HP = __import__('3-entropy').HP
-
-X = np.loadtxt("mnist2500_X.txt")
-X = pca(X, 50)
-D, P, betas, _ = P_init(X, 30.0)
-H0, P[0, 1:] = HP(D[0, 1:], betas[0])
-print(H0)
-print(P[0])
