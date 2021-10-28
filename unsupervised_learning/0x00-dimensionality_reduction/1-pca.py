b9290cf7dd3b63a4212ad6dc83d724c30b93106b
Merhbene <merhbeno@gmail.com>
2021-10-28 12:33:05 +0100

    pca

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def pca(X, ndim):
     """perform PCA on a dataset"""
+    # center the value in each column by subtracting the mean column value
     X = X - np.mean(X, axis=0)
     # Singular Value Decomposition:
     _, s, vh = np.linalg.svd(X)
