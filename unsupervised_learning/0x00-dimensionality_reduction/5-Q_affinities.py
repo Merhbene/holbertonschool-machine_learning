102155a6d67fa46a0d55755b347949299f2cf1c4
Merhbene <merhbeno@gmail.com>
2021-10-30 11:19:08 +0100

    Q affinities

---
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -0,0 +1,12 @@
+import numpy as np
+"""Q affinities"""
+
+
+def Q_affinities(Y):
+    """Calculates the Q affinities"""
+    # Y contain the low dimensional transformation of X
+    D = np.square(Y[:, None, :] - Y[None, :, :]).sum(axis=-1)
+    num = (1 + D) ** (-1)
+    np.fill_diagonal(num, 0)
+    Q = num / np.sum(num)
+    return Q, num
