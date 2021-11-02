6dea16efcf2f20e0945cbc0014a673ae22d96d8e
Merhbene <merhbeno@gmail.com>
2021-11-02 15:28:37 +0100

    k means

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def initialize(X, k):
     """ initializes cluster centroids for K-means"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    if type(k) is not int or k < 1:
+        return None
     _, d = X.shape
     low = np.amin(X, 0)
     high = np.amax(X, 0)
--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -4,29 +4,35 @@ import numpy as np
 
 
 def kmeans(X, k, iterations=1000):
-  "performs K-means on a dataset"
-  _, d = X.shape
-  mins = np.min(X, axis=0)
-  maxs = np.max(X, axis=0)
-  
-  centroids = np.random.uniform(mins, maxs, size=(k, d))
-  new_centroids = centroids.copy()
-  for i in range(iterations):
-      
-      dist = np.square(X[:, None, :] - centroids[None, :, :]).sum(axis=-1)
-      clss = np.argmin(dist, axis=1)
-      
+    "performs K-means on a dataset"
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None
+    if type(k) is not int  or k < 1:
+        return None, None
+    if type(iterations) is not int or int(iterations) != iterations or iterations < 1:
+        return None, None
+    _, d = X.shape
+    mins = np.min(X, axis=0)
+    maxs = np.max(X, axis=0)
+    
+    centroids = np.random.uniform(mins, maxs, size=(k, d))
+    new_centroids = centroids.copy()
+    for i in range(iterations):
+        
+        dist = np.square(X[:, None, :] - centroids[None, :, :]).sum(axis=-1)
+        clss = np.argmin(dist, axis=1)
+        
 
-      for c in range(k):
-          indices = np.argwhere(clss == c)
-          #centroids[indices] = np.mean(x[indices], axis=0)
-          if len(X[indices]) > 0:
-              new_centroids[c] = np.mean(X[indices], axis=0)
-          else:
-              new_centroids[c] = np.random.uniform(mins, maxs, size=d)
-          
+        for c in range(k):
+            indices = np.argwhere(clss == c)
+            #centroids[indices] = np.mean(x[indices], axis=0)
+            if len(X[indices]) > 0:
+                new_centroids[c] = np.mean(X[indices], axis=0)
+            else:
+                new_centroids[c] = np.random.uniform(mins, maxs, size=d)
+            
 
-      if np.array_equal(centroids, new_centroids):
-          break
-      centroids = new_centroids.copy()
-  return centroids, clss
+        if np.array_equal(centroids, new_centroids):
+            break
+        centroids = new_centroids.copy()
+    return centroids, clss
