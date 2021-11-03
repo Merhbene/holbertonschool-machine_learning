92bb1efb74e784b478ee24a32778062beceef69c
Merhbene <merhbeno@gmail.com>
2021-11-03 10:55:35 +0100

    optimum

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""optimum"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    "tests for the optimum number of clusters by variance"
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None
+    if type(kmin) is not int or kmin < 1:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0] 
+    if type(kmax) is not int or kmax < 1:
+        return None, None
+    if kmax <= kmin:
+        return None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None
+    results = []
+    var = []
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
+        var.append(variance(X, C))
+    d0 = var[0]
+    d_vars = []
+    d_vars = [d0 - v for v in var]
+    return results, d_vars
