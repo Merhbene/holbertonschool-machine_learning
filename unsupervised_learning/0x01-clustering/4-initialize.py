516868ee9b620a79d1210702261b9819d8d43a91
Merhbene <merhbeno@gmail.com>
2021-11-03 16:48:46 +0100

    GMM

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -6,11 +6,12 @@ kmeans = __import__('1-kmeans').kmeans
 
 def initialize(X, k):
     """initializes variables for a Gaussian Mixture Model"""
-    _,  d = X.shape
     if type(X) is not np.ndarray or X.ndim != 2:
         return None, None, None
     if type(k) is not int or k < 1:
         return None, None, None
+
+    _,  d = X.shape
     pi = np.full(k, 1/k) # a new array of shape k filled with 1/k
     m, _ = kmeans(X, k)
     # S = np.array([np.eye(d) for i in range(k)])   
