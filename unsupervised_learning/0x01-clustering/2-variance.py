1f5223abddefc72405bded7765039aaf1b5b560b
Merhbene <merhbeno@gmail.com>
2021-11-03 10:38:51 +0100

    variance

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -10,6 +10,6 @@ def variance(X, C):
     if type(C) is not np.ndarray or C.ndim != 2 or C.shape[1] != X.shape[1]:
         return None
     dist = np.square(X[:, None, :] - C[None, :, :]).sum(axis=-1)
-    centroid_dist = np.min(dist, axis=1)    
+    centroid_dist = np.min(dist, axis=1) 
     var = np.sum(centroid_dist)
     return var
