8df2e09c05e5d23f219269197bd12675a9158596
Merhbene <merhbeno@gmail.com>
2021-11-04 10:35:37 +0100

    pdf

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""pdf"""
+import numpy as np
+
+
+def pdf(X, m, S):
+    """calculate the pdf of the multinormal distribution"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    d = X.shape[1]
+    if type(m) is not np.ndarray or m.ndim != 1 or m.shape[0] != d:
+        return None
+    if type(S) is not np.ndarray or S.ndim != 2 or S.shape != (d, d):
+        return None
+
+    Xm = X - m
+    e = - 0.5 * np.sum(Xm * np.matmul(np.linalg.inv(S), Xm.T).T, axis=1)
+    num = np.exp(e)
+    det = np.linalg.det(S)
+    prob = num / np.sqrt(((2 * np.pi) ** d) * det)
+    return  np.maximum(prob, 1e-300) # should have a minimum value of 1e-300
+
+
+if __name__ == '__main__':
+ 
+    np.random.seed(0)
+    m = np.array([12, 30, 10])
+    S = np.array([[36, -30, 15], [-30, 100, -20], [15, -20, 25]])
+    X = np.random.multivariate_normal(m, S, 10000)
+    P = pdf(X, m, S)
+    print(P)
