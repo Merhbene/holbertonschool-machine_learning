2e2208acc840ee1e088dad356d734c7a729b9899
Merhbene <merhbeno@gmail.com>
2021-11-04 14:40:53 +0100

    main

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -35,3 +35,26 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         if verbose:
             print('Log Likelihood after {} iterations: {}'.format(iterations, l.round(5)))
     return pi, m, S, g, l
+
+import matplotlib.pyplot as plt
+import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
+
+if __name__ == '__main__':
+    np.random.seed(11)
+    a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
+    b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
+    c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
+    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    X = np.concatenate((a, b, c, d), axis=0)
+    np.random.shuffle(X)
+    k = 4
+    pi, m, S, g, l = expectation_maximization(X, k, 150, verbose=True)
+    clss = np.sum(g * np.arange(k).reshape(k, 1), axis=0)
+    plt.scatter(X[:, 0], X[:, 1], s=20, c=clss)
+    plt.scatter(m[:, 0], m[:, 1], s=50, c=np.arange(k), marker='*')
+    plt.show()
+    print(X.shape[0] * pi)
+    print(m)
+    print(S)
+    print(l)
