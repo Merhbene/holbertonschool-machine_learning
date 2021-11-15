e1d88fa75ddfa6ea1726605c5f6daf4364ae1c69
Merhbene <merhbeno@gmail.com>
2021-11-15 09:43:01 +0100

    multinormal

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,88 +1,35 @@
 #!/usr/bin/env python3
-"""Module contains MultiNormal Class"""
+"""
+Contains the class MultiNormal
+"""
 
 
 import numpy as np
 
 
-class MultiNormal():
-    """
-       Class which represents multivariate normal
-        distribution.
-    """
-
+class MultiNormal:
+    """represents a multinormal distribution"""
     def __init__(self, data):
-        """
-        Class Constructor
-        Args:
-            data: numpy.ndarray of shape (d, n)
-                n: Number of data points.
-                d: Number of dimensions in each data point.
-        Public Attributes:
-            mean: numpy.ndarray of shape (d, 1)
-            cov: numpy.ndarray of shape (d, d
-        """
+        """initializes a multinormal instance"""
         if type(data) is not np.ndarray or len(data.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
-        self.mean, self.cov = self.mean_cov(data.T)
-        """""
+            raise TypeError('data must be a 2D numpy.ndarray')
         n = data.shape[1]
+        if n < 2:
+            raise ValueError('data must contain multiple data points')
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = np.matmul((data - self.mean), (data - self.mean).T) / (n - 1)
-        """""
-    @staticmethod
-    def mean_cov(X):
-        """
-        Calculates the mean and covariance of
-            a dataset.
-        Args:
-            X: numpy.ndarray - shape (n, d) containing data set.
-                n: Number of data points.
-                d: Number of dimensions in each data point.
-        Returns: mean, cov:
-            mean: numpy.ndarray - shape (d, 1) containing the
-                mean of the data set.
-            cov: numpy.ndarray - shape (d, d) containing the
-                covariance matrix of the data set.
-        """
-        if type(X) is not np.ndarray or len(X.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
-
-        if X.shape[0] < 2:
-            raise ValueError("data must contain multiple data points")
-
-        n, d = X.shape
-        mean = X.sum(axis=0)/n
-        deviation = X - mean
-        covariant = np.matmul(deviation.T, deviation)
-        return mean.reshape((d, 1)), covariant/(n-1)
 
     def pdf(self, x):
-        """
-           Calculates multivariate pdf at data point.
-           Args:
-            x: numpy.ndarray - shape (d, 1) Data point.
-           Return:
-            Value of the pdf.
-        """
-        D = self.mean.shape[0]
-
+        """calculate the pdf of the multinormal distribution"""
         if type(x) is not np.ndarray:
-            raise TypeError("x must be a numpy.ndarray")
+            raise TypeError('x must be a numpy.ndarray')
+        if x.shape != self.mean.shape:
+            raise ValueError('x must have the shape {}'.format(self.mean.shape))
+        d = x.shape[0]
+        xm = x - self.mean
+        exponent = - 0.5 * np.matmul(xm.T, np.matmul(np.linalg.inv(self.cov), xm))
+        return np.exp(exponent[0, 0]) / np.sqrt(((2 * np.pi) ** d) * np.linalg.det(self.cov))
 
-        x1, x2 = x.shape
-
-        if len(x.shape) != 2 or x1 != D or x2 != 1:
-            raise ValueError(
-                "x must have the shape ({}, 1)".format(D)
-                )
-
-        Px = (2*np.pi)**(D/2)
-        Px = 1 / (Px * (np.linalg.det(self.cov)**(1/2)))
-        covI = np.linalg.inv(self.cov)
-        x_mu = x - self.mean
-        dot = np.dot(np.dot(x_mu.T, covI), x_mu)
-        return float(Px*np.exp((-1/2)*dot))
 
 if __name__ == '__main__':
     import numpy as np
