a995bd32e906f76e2d76ad9340e6b6edafb508d3
Merhbene <merhbeno@gmail.com>
2021-11-15 09:35:12 +0100

    test

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -24,50 +24,11 @@ class MultiNormal():
         """
         if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        self.mean, self.cov = self.mean_cov(data.T)
+        #self.mean, self.cov = self.mean_cov(data.T)
+        n = data.shape[1]
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        self.cov = np.matmul((data - self.mean), (data - self.mean).T) / (n - 1)
 
-    @staticmethod
-    def mean_cov(X):
-        """
-        Calculates the mean and covariance of
-            a dataset.
-        Args:
-            X: numpy.ndarray - shape (n, d) containing data set.
-                n: Number of data points.
-                d: Number of dimensions in each data point.
-        Returns: mean, cov:
-            mean: numpy.ndarray - shape (d, 1) containing the
-                mean of the data set.
-            cov: numpy.ndarray - shape (d, d) containing the
-                covariance matrix of the data set.
-        """
-        if type(X) is not np.ndarray or len(X.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
-
-        if X.shape[0] < 2:
-            raise ValueError("data must contain multiple data points")
-
-        n, d = X.shape
-        mean = X.sum(axis=0)/n
-        deviation = X - mean
-        covariant = np.matmul(deviation.T, deviation)
-        return mean.reshape((d, 1)), covariant/X.shape[0]
-
-    @staticmethod
-    def correlation(C):
-        """
-        Calculates a correlation matrix.
-        Args:
-            C: numpy.ndarray - Covariant matrix.
-        Return:
-            numpy.ndarray - Correlation matrix.
-        """
-
-        Di = np.sqrt(np.diag(C))
-        outer_Di = np.outer(Di, Di)
-        corr = C / outer_Di
-        corr[C == 0] = 0
-        return corr
 
     def pdf(self, x):
         """
@@ -95,3 +56,14 @@ class MultiNormal():
         x_mu = x - self.mean
         dot = np.dot(np.dot(x_mu.T, covI), x_mu)
         return float(Px*np.exp((-1/2)*dot))
+
+if __name__ == '__main__':
+    import numpy as np
+    from multinormal import MultiNormal
+
+    np.random.seed(0)
+    data = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000).T
+    mn = MultiNormal(data)
+    x = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 1).T
+    print(x)
+    print(mn.pdf(x))
