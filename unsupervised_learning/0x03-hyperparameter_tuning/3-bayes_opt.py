cf1c919b70e21302ba3d8754d0e1c674f472a6bb
Merhbene <merhbeno@gmail.com>
2021-11-16 16:17:18 +0100

    BO

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,43 @@
+"""Hyperparameter tuning module"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """BayesianOptimization class
+    """
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+        "Class constructor"
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.xsi = xsi
+        self.minimize = minimize
+    
+        min, max = bounds
+        X_s = np.linspace(min, max, ac_samples)
+        self.X_s = np.expand_dims(X_s, axis=1) # or X_s.reshape(-1, 1)
+
+GP = __import__('2-gp').GaussianProcess
+BO = __import__('3-bayes_opt').BayesianOptimization
+# import matplotlib.pyplot as plt
+
+
+def f(x):
+    """our 'black box' function"""
+    return np.sin(5*x) + 2*np.sin(-2*x)
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    X_init = np.random.uniform(-np.pi, 2*np.pi, (2, 1))
+    Y_init = f(X_init)
+
+    bo = BO(f, X_init, Y_init, (-np.pi, 2*np.pi), 50, l=2, sigma_f=3, xsi=0.05)
+    print(bo.f is f)
+    print(type(bo.gp) is GP)
+    print(bo.gp.X is X_init)
+    print(bo.gp.Y is Y_init)
+    print(bo.gp.l)
+    print(bo.gp.sigma_f)
+    print(bo.X_s.shape, bo.X_s)
+    print(bo.xsi)
+    print(bo.minimize)
