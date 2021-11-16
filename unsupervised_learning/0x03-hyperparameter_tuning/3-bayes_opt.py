571956ed3896cf87a4458e4e891eeff12e6392c8
Merhbene <merhbeno@gmail.com>
2021-11-16 16:19:45 +0100

    BO

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -17,27 +17,3 @@ class BayesianOptimization:
         min, max = bounds
         X_s = np.linspace(min, max, ac_samples)
         self.X_s = np.expand_dims(X_s, axis=1) # or X_s.reshape(-1, 1)
-
-# BO = __import__('3-bayes_opt').BayesianOptimization
-# import matplotlib.pyplot as plt
-
-
-def f(x):
-    """our 'black box' function"""
-    return np.sin(5*x) + 2*np.sin(-2*x)
-
-if __name__ == '__main__':
-    np.random.seed(0)
-    X_init = np.random.uniform(-np.pi, 2*np.pi, (2, 1))
-    Y_init = f(X_init)
-
-    bo = BO(f, X_init, Y_init, (-np.pi, 2*np.pi), 50, l=2, sigma_f=3, xsi=0.05)
-    print(bo.f is f)
-    print(type(bo.gp) is GP)
-    print(bo.gp.X is X_init)
-    print(bo.gp.Y is Y_init)
-    print(bo.gp.l)
-    print(bo.gp.sigma_f)
-    print(bo.X_s.shape, bo.X_s)
-    print(bo.xsi)
-    print(bo.minimize)
