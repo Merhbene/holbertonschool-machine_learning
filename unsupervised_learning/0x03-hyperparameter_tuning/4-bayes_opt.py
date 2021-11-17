f9edc19a6fc4f3c6acb291100d5f9d30bc020c8b
Merhbene <merhbeno@gmail.com>
2021-11-17 12:12:57 +0100

    acquisition fct

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -8,7 +8,8 @@ GP = __import__('2-gp').GaussianProcess
 class BayesianOptimization:
     """BayesianOptimization class
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
+                 xsi=0.01, minimize=True):
         "Class constructor"
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
