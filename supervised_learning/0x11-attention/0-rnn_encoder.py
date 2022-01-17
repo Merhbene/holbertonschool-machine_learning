1d36d8d0bafc9af8d1990a31597de4437ea010ef
Merhbene <merhbeno@gmail.com>
2022-01-17 13:45:36 +0100

    rnn_encoder

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -2,15 +2,18 @@
 import tensorflow as tf
 
 
-class RNNEncoder:
+class RNNEncoder(tf.keras.layers.Layer):
+
     def __init__(self, vocab, embedding, units, batch):
         self.batch = batch #  the batch size
         self.units = units # the number of hidden units in the RNN cell
         self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
-        self.gru = tf.keras.layers.GRU(units=units,  return_sequences=True, return_state=True)
+        self.gru = tf.keras.layers.GRU(units=units,  return_sequences=True, return_state=True, recurrent_initializer="glorot_uniform")
+
     def initialize_hidden_state(self):
         initializer = tf.keras.initializers.Zeros()
         return initializer(shape=(self.batch, self.units))
+
     def call(self, x, initial):
         embeds = self.embedding(x)
         outputs, hidden = self.gru(inputs=embeds, initial_state=initial)   
