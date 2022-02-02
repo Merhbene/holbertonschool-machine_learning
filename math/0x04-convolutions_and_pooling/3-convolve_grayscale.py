487e9c896b5cb6227ecc6ad3a790b36397535453
Merhbene <merhbeno@gmail.com>
2022-02-02 16:11:13 +0100

    test

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -7,22 +7,22 @@ import numpy as np
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
-    m, input_h, input_w = images.shape
-    kernel_h, kernel_w = kernel.shape
+    m, h, w = images.shape
+    kh, kw = kernel.shape
     sh, sw = stride
 
     if padding == "valid":
         ph = 0
         pw = 0
     elif padding == "same":
-        ph = int(np.ceil((input_h - 1) * sh + kernel_h - input_h) / 2)
-        pw = int(np.ceil((input_w - 1) * sw + kernel_w - input_w) / 2)
+        ph = int(np.ceil((h - 1) * sh + kh - h) / 2)
+        pw = int(np.ceil((w - 1) * sw + kw - w) / 2)
     else:
         ph = padding[0]
         pw = padding[1]
 
-    output_height = int(((input_h + 2 * ph - kernel_h) / sh)) + 1
-    output_width = int(((input_w + 2 * pw - kernel_w) / sw)) + 1
+    output_height = int(((h + 2 * ph - kh) / sh)) + 1
+    output_width = int(((w + 2 * pw - kw) / sw)) + 1
 
     # convolution output
     output = np.zeros((m, output_height, output_width))
@@ -38,6 +38,6 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
             x = i * sh
             y = j * sw
             # element-wise multiplication of the kernel and the image
-            img_slice = image_padded[:, x:x+kernel_h, y:y+kernel_w]
+            img_slice = image_padded[:, x:x+kh, y:y+kw]
             output[:, i, j] = np.tensordot(img_slice, kernel)
     return output
