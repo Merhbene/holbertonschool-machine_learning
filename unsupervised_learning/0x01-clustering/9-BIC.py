28224c128209b4ecd38e3324bd45a7ff99100b3a
Merhbene <merhbeno@gmail.com>
2022-04-07 09:57:09 +0100

    Condt

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -3,7 +3,7 @@
 #!/usr/bin/env python3
 """ Defines `BIC`. """
 import numpy as np
-#expectation_maximization = __import__('8-EM').expectation_maximization
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
