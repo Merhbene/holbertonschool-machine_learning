1a8f8e824288bb2ab753c3f88b67a8f8e94f14fe
Merhbene <merhbeno@gmail.com>
2022-04-07 14:49:41 +0100

    re

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,84 +1,69 @@
 #!/usr/bin/env python3
-"""Defines BIC"""
+""" BIC """
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Finds the best number of clusters for a GMM using the Bayesian Information
-    Criterion.
-    X: A numpy.ndarray of shape (n, d) containing the data set.
-    kmin: A positive integer containing the minimum number of clusters to check
-        for (inclusive).
-    kmax: A positive integer containing the maximum number of clusters to check
-        for (inclusive).
-    iterations: A positive integer containing the maximum number of iterations
-        for the EM algorithm.
-    tol: A non-negative float containing the tolerance for the EM algorithm.
-    verbose: A boolean that determines if the EM algorithm should print
-        information to the standard output.
-    Returns: (best_k, best_result, l, b) on success, or (None, None, None,
-        None) on failure:
-        best_k: The best value for k based on its BIC.
-        best_result: A tuple containing (pi, m, S).
-            pi: A numpy.ndarray of shape (k,) containing the cluster priors for
-                the best number of clusters.
-            m: A numpy.ndarray of shape (k, d) containing the centroid means
-                for the best number of clusters.
-            S: A numpy.ndarray of shape (k, d, d) containing the covariance
-                matrices for the best number of clusters.
-        l: A numpy.ndarray of shape (kmax - kmin + 1) containing the log
-            likelihood for each cluster size tested.
-        b: A numpy.ndarray of shape (kmax - kmin + 1) containing the BIC value
-            for each cluster size tested.
-    """
-    fail = (None, None, None, None)
-    if (
-            type(X) is not np.ndarray or
-            len(X.shape) != 2 or
-            type(kmin) is not int or
-            kmin < 1
-            ):
-        return fail
-    sample_count, dimention_count = X.shape
+    """calculates BIC over various """
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None, None, None
+    n, d = X.shape
+    if type(kmin) is not int or kmin != int(kmin) or kmin < 1:
+        return None, None, None, None
     if kmax is None:
-        kmax = sample_count
-    if (
-            type(kmax) is not int or
-            kmax < 1 or
-            kmax < kmin + 1 or
-            type(iterations) is not int or
-            iterations < 1 or
-            type(tol) is not float or
-            tol < 0 or
-            type(verbose) is not bool
-            ):
-        return fail
+        kmax = n 
+    if type(kmax) is not int or kmax != int(kmax) or kmax < 1:
+        return None, None, None, None
+    if kmax <= kmin:
+        return None, None, None, None
+    if type(iterations) is not int or iterations != int(iterations) or iterations < 1:
+        return None, None, None, None
+    if type(tol) is not float or tol < 0:
+        return None, None, None, None
+    if type(verbose) is not bool:
+        return None, None, None, None
 
+    b = np.zeros(kmax + 1 - kmin)
+    l = np.zeros(kmax + 1 - kmin)
     results = []
-    log_likelihoods = []
-    BICs = []
-    for cluster_count in range(kmin, kmax + 1):
-        priors, centroids, covariances, responsibilities, log_likelihood = \
-            expectation_maximization(
-                X, cluster_count, iterations, tol, verbose)
-        results.append((priors, centroids, covariances))
-        log_likelihoods.append(log_likelihood)
-        parameter_count = (
-            cluster_count * (dimention_count + 2) * (dimention_count + 1) / 2
-            - 1
-        )
-        BICs.append(
-             np.log(sample_count) * parameter_count - 2 * log_likelihood)
 
-    best_index = np.argmin(BICs)
-    best_cluster_count = kmin + best_index
-    best_parameters = results[best_index]
+    for k in range(kmin, kmax + 1):
+        pi, m, S, _, l[k - kmin] = expectation_maximization(X, k, iterations=iterations, tol=tol, verbose=verbose)
+        results.append((pi, m, S))
+        p = k * (d + 2) * (d + 1) / 2 - 1
+        b[k - kmin] = p * np.log(n) - 2 * l[k - kmin]
 
-    return (
-        best_cluster_count,
-        best_parameters,
-        np.array(log_likelihoods),
-        np.array(BICs)
-    )
+    amin = np.argmin(b)
+    best_k = amin + kmin
+    best_result = results[amin]
+    return best_k, best_result, l, b
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    np.random.seed(11)
+    a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
+    b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
+    c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
+    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    X = np.concatenate((a, b, c, d), axis=0)
+    np.random.shuffle(X)
+    best_k, best_result, l, b = BIC(X, kmin=1, kmax=10)
+    print(best_k)
+    print(best_result)
+    print(l)
+    print(b)
+    x = np.arange(1, 11)
+    plt.plot(x, l, 'r')
+    plt.xlabel('Clusters')
+    plt.ylabel('Log Likelihood')
+    plt.tight_layout()
+    plt.show()
+    plt.plot(x, b, 'b')
+    plt.xlabel('Clusters')
+    plt.ylabel('BIC')
+    plt.tight_layout()
+    plt.show()
