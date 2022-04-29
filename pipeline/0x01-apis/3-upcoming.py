2314fe7f9a94feec96a2ee6fd23b53b396b497d4
Merhbene <merhbeno@gmail.com>
2022-04-29 13:46:28 +0100

    try

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -6,16 +6,5 @@ import requests
 
 if __name__ == '__main__':
     base = "https://api.spacexdata.com/v4/"
-    info = requests.get(base+"launches/next").json()
-    date = info["date_local"]
-    name = info["name"]
-    launchpad_id = "launchpads/"+info["launchpad"]
-    rocket_id = "rockets/"+info["rocket"]
-    rocket_info = requests.get(base+rocket_id).json()
-    launchpad_info = requests.get(base+launchpad_id).json()
-    rocket = rocket_info["name"]
-    launchpad = launchpad_info["name"]
-    pad_location = launchpad_info["locality"]
-    args = (name, date, rocket, launchpad, pad_location)
-    result = "{} ({}) {} - {} ({})".format(args)
-    print(result)
+
+    print(base)
