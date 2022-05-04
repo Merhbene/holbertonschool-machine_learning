ac52b38e1f7f8f5d375015bd23a7d759b7683361
Merhbene <merhbeno@gmail.com>
2022-05-04 17:32:32 +0100

    yos

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python3
-"""API module"""
+'''3. What will be next?'''
 import requests
-from datetime import datetime
+
+
+def get_launches():
+    '''return launches'''
+    url = 'https://api.spacexdata.com/v4/launches/upcoming'
+    request = requests.get(url).json()
+    request.sort(key=lambda json: json['date_unix'])
+    latest = request[0]
+    launch_name = latest['name']
+    launch_date = latest['date_local']
+    rocket_name = requests.get(
+        'https://api.spacexdata.com/v4/rockets/' + latest['rocket']
+    ).json()['name']
+    launchpad = requests.get(
+        'https://api.spacexdata.com/v4/launchpads/' + latest['launchpad']
+    ).json()
+    launchpad_name = launchpad['name']
+    launchpad_locality = launchpad['locality']
+    print('{} ({}) {} - {} ({})'.format(
+        launch_name,
+        launch_date,
+        rocket_name,
+        launchpad_name,
+        launchpad_locality
+    ))
 
 
 if __name__ == '__main__':
-    base_url = "https://api.spacexdata.com/v4"
-
-    response = requests.get(base_url + "/launches/upcoming")
-    content = response.json()
-    upcoming_launch = tuple()
-
-    for launch in content:
-        launch_name = launch['name']
-        launch_date = launch['date_unix']
-        rocket_id = launch['rocket']
-        lauchpad_id = launch['launchpad']
-        if len(upcoming_launch) == 0 or launch_date < upcoming_launch[0]:
-            upcoming_launch = (
-                launch_date,
-                launch_name,
-                rocket_id,
-                lauchpad_id,
-            )
-
-    launch_name = upcoming_launch[1]
-    launch_date = datetime.fromtimestamp(upcoming_launch[0]).isoformat()
-
-    response = requests.get(base_url + "/rockets/" + upcoming_launch[2])
-    rocket_name = response.json()['name']
-
-    response = requests.get(base_url + "/launchpads/" + upcoming_launch[3])
-    launchpad_name = response.json()['name']
-    launchpad_loc = response.json()['locality']
-
-    print(
-        "{} ({}) {} - {} ({})".format(
-            launch_name,
-            launch_date,
-            rocket_name,
-            launchpad_name,
-            launchpad_loc
-        )
-    )
+    get_launches()
