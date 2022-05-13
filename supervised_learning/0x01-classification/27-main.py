97b8d611d14379de4a33416b1b46f13072cd9ef9
Merhbene <merhbeno@gmail.com>
2022-05-13 11:43:19 +0100

    main 26

--- supervised_learning/0x01-classification/27-main.py
+++ supervised_learning/0x01-classification/27-main.py
@@ -1,4 +1,4 @@
-
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,7 +6,7 @@ Deep = __import__('27-deep_neural_network').DeepNeuralNetwork
 one_hot_encode = __import__('24-one_hot_encode').one_hot_encode
 one_hot_decode = __import__('25-one_hot_decode').one_hot_decode
 
-lib= np.load('../data/MNIST.npz')
+lib= np.load('.\data\MNIST.npz')
 X_train_3D = lib['X_train']
 Y_train = lib['Y_train']
 X_valid_3D = lib['X_valid']
@@ -40,4 +40,29 @@ for i in range(100):
     plt.axis('off')
 plt.tight_layout()
 plt.show()
+"""
+import numpy as np
+Deep = __import__('27-deep_neural_network').DeepNeuralNetwork
 
+def one_hot(Y, classes):
+    """convert an array to a one hot encoding"""
+    oh = np.zeros((classes, Y.shape[0]))
+    oh[Y, np.arange(Y.shape[0])] = 1
+    return oh
+
+np.random.seed(5)
+nx, m = np.random.randint(100, 200, 2).tolist()
+classes = np.random.randint(5, 20)
+X = np.random.randn(nx, m)
+Y = one_hot(np.random.randint(0, classes, m), classes)
+
+deep = Deep(nx, [100, 50, classes])
+A, cost = deep.train(X, Y, iterations=10, graph=False, verbose=False)
+A = A.astype(float)
+print(np.round(A, 10))
+print(np.round(cost, 10))
+print(deep.L)
+for k, v in sorted(deep.cache.items()):
+    print(k, np.round(v, 10))
+for k, v in sorted(deep.weights.items()):
+    print(k, np.round(v, 10))
