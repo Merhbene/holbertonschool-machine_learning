372aadce3742f9f3b20a82ec0de3e96cdc07bce3
Oumaima Merhbene <49324230+Merhbene@users.noreply.github.com>
2022-08-26 21:14:04 +0100

    Update 6-train.py

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-"""
-this module contain train function
-"""
-import tensorflow.compat.v1 as tf
+"""Tensorflow module"""
+import tensorflow as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
@@ -10,89 +8,50 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """
-    builds, trains, and saves a neural network classifier:
-    X_train is a numpy.ndarray containing the training input data
-    Y_train is a numpy.ndarray containing the training labels
-    X_valid is a numpy.ndarray containing the validation input data
-    Y_valid is a numpy.ndarray containing the validation labels
-        *layer_sizes is a list containing the number of
-        nodes in each layer of the network
-        *activations is a list containing the
-        activation functions for each layer of the network
-    alpha is the learning rate
-    iterations is the number of iterations to train over
-    save_path designates where to save the model
-    Add the following to the graph’s collection
-    placeholders: x and y
-    tensors: y_pred, loss and accuracy
-    operation: train_op
-    """
-    # x: is the placeholder for the input data
-    # to the neural network
-    # y: is the placeholder for the one-hot labels
-    # for the input data
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-
-    # TENSORS:
-
-    # y_pred: the prediction of the network in tensor form
+def train(
+        X_train,
+        Y_train,
+        X_valid,
+        Y_valid,
+        layer_sizes,
+        activations,
+        alpha,
+        iterations,
+        save_path="/tmp/model.ckpt"
+):
+    """Builds, trains, and saves neural network classifier"""
+    nx = X_train.shape[1]
+    classes = Y_train.shape[1]
+    x, y = create_placeholders(nx, classes)
+    tf.add_to_collection("x", x)
+    tf.add_to_collection("y", y)
     y_pred = forward_prop(x, layer_sizes, activations)
-    # tensor containing the decimal accuracy of the prediction (mean)
+    tf.add_to_collection("y_pred", y_pred)
     accuracy = calculate_accuracy(y, y_pred)
-    # tensor containing the loss of the prediction
+    tf.add_to_collection("accuracy", accuracy)
     loss = calculate_loss(y, y_pred)
-
-    # operation that trains the network using gradient descent
+    tf.add_to_collection("loss", loss)
     train_op = create_train_op(loss, alpha)
-
-    tf.add_to_collection('x', x)
-    tf.add_to_collection('y', y)
-    tf.add_to_collection('y_pred', y_pred)
-    tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
-
-    # instance of tf.train.Saver() to save
-    saver = tf.train.Saver()
-
-    # allocates the memory for the Variable and sets its initial values.
+    tf.add_to_collection("train_op", train_op)
     init = tf.global_variables_initializer()
-
-    # tf.Session object encapsulates the environment
-    # in which Operation objects are executed
-
-    # and Tensor objects are evaluated
+    saver = tf.train.Saver()
     with tf.Session() as session:
         session.run(init)
-
-        for i in range(iterations + 1):
-            # foward propagation
-            lossTrain = session.run(loss,
-                                    feed_dict={x: X_train, y: Y_train})
-            lossValid = session.run(loss,
-                                    feed_dict={x: X_valid, y: Y_valid})
-
-            # back propagation
-            accuracyTrain = session.run(accuracy,
-                                        feed_dict={x: X_train, y: Y_train})
-            accuracyValid = session.run(accuracy,
-                                        feed_dict={x: X_valid, y: Y_valid})
-            if i % 100 == 0 or i == iterations:
-                print("After {} iterations:".format(i))
-                print("\tTraining Cost: {}".format(lossTrain))
-                print("\tTraining Accuracy: {}".format(accuracyTrain))
-                print("\tValidation Cost: {}".format(lossValid))
-                print("\tValidation Accuracy: {}".format(accuracyValid))
-             if iteration < iterations:
-                #session.run(train_op, feed_dict={x: X_train, y: Y_train})
-                session.run(train_op,
-                            feed_dict={x: X_train, y: Y_train})
-
-        # This method runs the ops added by the constructor
-        # for saving variables.
-        # It requires a session in which the graph was launched.
-        # The variables to save must also have been initialized.
+        for iteration in range(iterations + 1):
+            train_cost, train_accuracy = session.run(
+                [loss, accuracy],
+                feed_dict={x: X_train, y: Y_train}
+            )
+            valid_cost, valid_accuracy = session.run(
+                [loss, accuracy],
+                feed_dict={x: X_valid, y: Y_valid}
+            )
+            if iteration % 100 == 0 or iteration == iterations:
+                print("After {} iterations:".format(iteration))
+                print("\tTraining Cost: {}".format(train_cost))
+                print("\tTraining Accuracy: {}".format(train_accuracy))
+                print("\tValidation Cost: {}".format(valid_cost))
+                print("\tValidation Accuracy: {}".format(valid_accuracy))
+            if iteration < iterations:
+                session.run(train_op, feed_dict={x: X_train, y: Y_train})
         return saver.save(session, save_path)
